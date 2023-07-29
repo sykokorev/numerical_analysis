e2cc615684e4a7006cb426b7468e289081b933de
sykokorev <skokorev1981@gmail.com>
2023-07-29 15:03:01 +0300

    Finished chapter 5.2 Direct methods solving system of linear equations. Added solving by LU Decomposition, Cholesky fractorization.

--- ch5/ch5_2.py
+++ ch5/ch5_2.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import numpy as np
+import scipy as sp
 
 
 DIR = os.path.abspath(os.path.join(
@@ -46,5 +47,51 @@ if __name__ == "__main__":
 
     # Gauss-Jordan Elimination
     x = gauss_jordan(a, b)
-    print([round(xi, 3) for xi in x])
-    print(np.linalg.solve(a, b))
+    # print([round(xi, 3) for xi in x])
+    # print(np.linalg.solve(a, b))
+
+    a = [
+        [2, 1, 1],
+        [5, 2, 2],
+        [4, 3, 2]
+    ]
+
+    b = [5, 6, 3]
+
+    print('Initial matrix')
+    for ai in a:
+        print(ai)
+
+    lower, upper = lu(a)
+
+    print('Upper triangular matrix')
+    for ui in upper:
+        print(ui)
+
+    print('Lower triangular matrix')
+    for li in lower:
+        print(li)
+    
+    print('Checking LU by multiply L * U')
+    init = mat_mul(lower, upper)
+    for i in init:
+        print(i)
+
+    # Solving equation
+    x = solveLU(a, b)
+    for i, xi in enumerate(x, 1):
+        print(f'x{i} = {xi}', end='\t')
+    print()
+
+    # Cholesky Factorization
+    a = [
+        [1, 3, 2],
+        [3, 13, 8],
+        [2, 8, 6]
+    ]
+    b = [2, 4, 1]
+
+    x = cholab(a, b)
+    for i, xi in enumerate(x, 1):
+        print(f'x{i} = {xi}', end='\t')
+    print()
--- la/linalg.py
+++ la/linalg.py
@@ -1,5 +1,7 @@
 import sys
 
+from typing import Tuple, List
+
 
 from la.linear_algebra import *
 
@@ -62,4 +64,74 @@ def gauss_jordan(a, b):
 
     x = [ci[-1] for ci in c]
 
+    return x
+
+
+# LU Decomposition. Return tuple(L, U)
+def lu(a):
+
+    width = len(a)
+    upper = deepcopy(a)
+    lower = identity(width)
+
+    for k in range(width):
+        
+        pivot = upper[k][k]
+        for i in range(k+1, width):
+            if k != i:
+                ratio = upper[i][k] / pivot
+                for j in range(width):
+                    upper[i][j] = upper[i][j] - upper[k][j] * ratio
+                lower[i][k] = ratio
+
+    return (lower, upper)    
+
+
+def solveLU(a, b):
+
+    width = len(a)
+    l, u = lu(a)
+    y = [0.0 for i in range(width)]
+    x = [0.0 for i in range(width)]
+
+    # Forward substitution [L]{y} = {b}, {y} = [U]{x}
+    for i in range(width):
+        y[i] = (1 / l[i][i]) * (b[i] - sum(l[i][j] * y[j] for j in range(i)))
+
+    # Backward substitution [L]{x} = {y}
+    for i in range(width-1, -1, -1):
+        x[i] = (1 / u[i][i]) * (y[i] - sum(u[i][j] * x[j] for j in range(i+1, width)))
+
+    return x
+
+
+def cholesky(a):
+
+    width = len(a)
+    u = [[0.0 for i in range(width)] for j in range(width)]
+
+    for i in range(width):
+        u[i][i] = (a[i][i] - sum([u[k][i] ** 2 for k in range(i)])) ** 0.5
+        for j in range(i+1, width):
+            u[i][j] = (1 / u[i][i]) * (a[i][j] - sum([u[k][i] * u[k][j] for k in range(i)]))
+   
+    return u
+
+
+def cholab(a, b):
+
+    width = len(a)
+    u = cholesky(a)
+    ut = transpose(u)
+    y = [0.0 for i in range(width)]
+    x = [0.0 for i in range(width)]
+
+    # Forward substitution 
+    for i in range(width):
+        y[i] = (1 / ut[i][i]) * (b[i] - sum(ut[i][j] * y[j] for j in range(i)))
+
+    # Backward substitution
+    for i in range(width-1, -1, -1):
+        x[i] = (1 / u[i][i]) * (y[i] - sum(u[i][j] * x[j] for j in range(i+1, width)))
+    
     return x
